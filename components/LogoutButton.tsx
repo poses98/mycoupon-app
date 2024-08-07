4a1019a1712b8954e336578de8fe7695e00882a9
poses98 <posesand@gmail.com>
2024-08-07 16:11:37 +0200

    feat: Add LogoutButton component and styles

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
+import useAuth from '@/hooks/useAuth';
+import { router } from 'expo-router';
+
+const LogoutButton = () => {
+  const { signOut } = useAuth();
+  return (
+    <TouchableOpacity
+      style={styles.logoutContainer}
+      onPress={async () => {
+        await signOut();
+        router.navigate('/sign-in');
+      }}
+    >
+      <Text style={styles.logoutText}>Logout</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  logoutText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  logoutContainer: {
+    width: '90%',
+    margin: 'auto',
+    alignItems: 'center',
+    borderColor: 'rgba(255,255,255,1)',
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+});
+
+export default LogoutButton;
