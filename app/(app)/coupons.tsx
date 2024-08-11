473c67098405ea6c1747d0284f9bcb9b5ca28aa1
poses98 <posesand@gmail.com>
2024-08-11 18:45:59 +0200

    chore: Refactor Coupons component to improve performance and readability

--- app/(app)/coupons.tsx
+++ app/(app)/coupons.tsx
@@ -1,54 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, StyleSheet, Alert } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Button from '@/components/Button';
 import { Colors } from '@/constants/Colors';
 import CouponApi from '@/api/coupon';
-
-interface ICoupon {
-  title: String;
-  description: String;
-  valid_from: Date;
-  valid_until: Date;
-  terms: String;
-  status: String;
-  created_at: Date;
-  validated_by: String;
-  is_valid_at: Array<String>;
-  redeemed_at: Date;
-  created_by: String;
-  redeemed_date: Date;
-}
+import ICoupon from '@/interfaces/ICoupon';
+import Loader from '@/components/Loader';
+import CouponContainer from '@/components/CouponContainer';
 
 export default function Coupons() {
+  const [isLoading, setIsLoading] = useState(true);
   const [coupons, setCoupons] = useState<Array<ICoupon>>([]);
+  const [couponsByDate, setCouponsByDate] = useState<any>([]);
 
   useEffect(() => {
-    console.log('Getting coupons');
-    const coupons = await CouponApi.getCoupons().then((response) => {
-      setCoupons(response.data);
-    });
+    const getCoupons = async () => {
+      const couponsFetch = await CouponApi.getCoupons();
+      if (couponsFetch.length > 0) {
+        setIsLoading(false);
+        setCoupons(couponsFetch);
+      }
+    };
+    getCoupons();
   }, []);
 
   useEffect(() => {
-    if (coupons) Alert.alert('Coupon', JSON.stringify(coupons[0]));
+    if (coupons.length > 0) {
+      const couponDate: { [date: string]: Array<ICoupon> } = {};
+      coupons.forEach((coupon) => {
+        const date = coupon.created_at.toString().split('T')[0];
+        if (!couponDate[date]) {
+          couponDate[date] = [];
+        }
+        couponDate[date].push(coupon);
+      });
+      setCouponsByDate(couponDate);
+    }
   }, [coupons]);
 
   return (
     <View style={styles.wrapper}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.wrapper}>
+      {isLoading && <Loader />}
+      {!isLoading && coupons.length > 0 && (
+        <ScrollView contentContainerStyle={styles.container}>
           <Button
             title="GENERAR CUPONES"
             onPress={() => {
+              /**TODO Open modal */
               console.log('Create coupon pressed');
             }}
             bgcolor="#fff"
             borderColor={Colors.light.tint}
             textColor={Colors.light.tint}
             borderRadius={11}
+            marginVertical={10}
           />
+          {Object.entries(couponsByDate).map(([date, couponsInADay]: any) => (
+            <CouponContainer
+              key={date}
+              date={date}
+              couponsInADay={couponsInADay}
+            />
+          ))}
+        </ScrollView>
+      )}
+      {!isLoading && coupons.length === 0 && (
+        <View>
+          <Text>No hay cupones</Text>
         </View>
-      </ScrollView>
+      )}
     </View>
   );
 }
@@ -56,16 +75,35 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     height: '100%',
-    backgroundColor: 'transparent',
+    width: '100%',
+    flexDirection: 'column',
+    flexGrow: 1,
+    paddingBottom: 20,
   },
   container: {
     width: '100%',
-    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    alignContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 14,
+    justifyContent: 'center',
+  },
+  couponContainer: {
+    width: '100%',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    alignContent: 'flex-start',
+    alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    padding: 10,
+  },
+  dateBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    backgroundColor: Colors.light.inputTextBackground,
+    borderRadius: 11,
+    marginBottom: 4,
   },
 });
