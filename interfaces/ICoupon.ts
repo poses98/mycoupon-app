13d05381628b4e95ce3410d29f566d0727af7485
poses98 <posesand@gmail.com>
2024-08-11 18:46:45 +0200

    Add ICoupon interface for defining the structure of a coupon

---
+++ interfaces/ICoupon.ts
@@ -0,0 +1,15 @@
+export default interface ICoupon {
+  _id: string;
+  title: string;
+  description: string;
+  valid_from: Date;
+  valid_until: Date;
+  terms: string;
+  status: string;
+  created_at: Date;
+  validated_by: string;
+  is_valid_at: Array<string>;
+  redeemed_at: Date;
+  created_by: string;
+  redeemed_date: Date;
+}
