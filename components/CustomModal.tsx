8fc146831a9326a2fc4b0aad9453f37f17e253c6
poses98 <posesand@gmail.com>
2024-08-13 12:13:43 +0200

    refactor: Update CustomModal component to include transparent background and box shadow

--- components/CustomModal.tsx
+++ components/CustomModal.tsx
@@ -24,22 +24,29 @@ export default function CustomModal({
       visible={isVisible}
       onRequestClose={onClose}
     >
-      <View style={[styles.modalContent, height ? { height: 'auto' } : {}]}>
-        <View style={styles.titleContainer}>
-          <ThemedText type="subtitle" style={styles.title}>
-            {title}
-          </ThemedText>
-          <Pressable onPress={onClose}>
-            <MaterialIcons name="close" color="#000" size={22} />
-          </Pressable>
+      <Pressable style={styles.modalBackground} onPress={onClose}>
+        <View style={[styles.modalContent, height ? { height: 'auto' } : {}]}>
+          <View style={styles.titleContainer}>
+            <ThemedText type="subtitle" style={styles.title}>
+              {title}
+            </ThemedText>
+            <Pressable onPress={onClose}>
+              <MaterialIcons name="close" color="#000" size={22} />
+            </Pressable>
+          </View>
+          {children}
         </View>
-        {children}
-      </View>
+      </Pressable>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent',
+  },
   modalContent: {
     height: '90%',
     width: '100%',
@@ -49,6 +56,13 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: 'white',
     paddingBottom: 20,
+    // Box shadow for iOS
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
+    // Box shadow for Android
+    elevation: 5,
   },
   titleContainer: {
     height: 50,
