a20b000c01b50ea2d4bd050febd976558660321e
poses98 <posesand@gmail.com>
2024-08-13 11:53:27 +0200

    refactor: Update CouponVisualizer component to display coupon details
    This commit updates the CouponVisualizer component to display the details of a coupon. It adjusts the size of the QR code, adds the coupon title, description, and event information, and improves the overall layout and styling. These changes enhance the user experience by providing a more comprehensive view of the coupon's information.

--- components/CouponVisualizer.tsx
+++ components/CouponVisualizer.tsx
@@ -12,31 +12,39 @@ export default function CouponVisualizer({ coupon }: { coupon: ICoupon }) {
   const redeemed = coupon.status === CouponStatus.REDEEMED;
   const statusText = redeemed ? 'CANJEADO' : 'NO CANJEADO';
   const memoizedQRCode = useMemo(() => {
-    return <QRCode value={coupon._id} size={50} />;
+    return <QRCode value={coupon._id} size={180} />;
   }, [coupon._id]);
-  console.log('Rendering coupon');
 
   return (
     <View style={styles.container}>
-      <View>
+      {/*  <View>
         <AntDesign
-          name="dropbox"
+          name="delete"
           size={24}
           color={Colors.light.redeemedCoupon}
         />
+      </View> */}
+      <View style={styles.imageContainer}>
+        <ThemedText
+          type="subtitle"
+          style={redeemed ? styles.redeemedText : styles.notRedeemedText}
+        >
+          {statusText}
+        </ThemedText>
+        {memoizedQRCode}
+        <ThemedText type="form-label">{coupon._id}</ThemedText>
       </View>
-      <ThemedText
-        type="subtitle"
-        style={redeemed ? styles.redeemedText : styles.notRedeemedText}
-      >
-        {statusText}
-      </ThemedText>
-      {memoizedQRCode}
-      <ThemedText type="form-label">{coupon._id}</ThemedText>
-      <ThemedText type="defaultSemiBold">{coupon.title}</ThemedText>
-      <ThemedText type="default">{coupon.description}</ThemedText>
-      <ThemedText type="form-label">Evento:</ThemedText>
-      <ThemedText type="default">{coupon.event}</ThemedText>
+      <View style={styles.infoContainer}>
+        <ThemedText type="defaultSemiBold" style={styles.title}>
+          {coupon.title}
+        </ThemedText>
+        <ThemedText type="default" style={styles.description}>
+          {coupon.description}
+        </ThemedText>
+        <ThemedText type="form-label">Evento</ThemedText>
+        <ThemedText type="default">{coupon.event}</ThemedText>
+      </View>
+
       <Button title="COMPARTIR" onPress={() => console.log('COMPARTIR')} />
     </View>
   );
@@ -46,8 +54,31 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     padding: 20,
+    flex: 1,
     flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
   },
   redeemedText: { color: Colors.light.redeemedCoupon },
   notRedeemedText: { color: Colors.light.tint },
+  infoContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 30,
+    lineHeight: 40,
+  },
+  description: {
+    fontSize: 17,
+    textAlign: 'left',
+  },
 });
