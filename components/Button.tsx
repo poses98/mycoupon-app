7039b437612113231ceb60488d830e9f012f0350
poses98 <posesand@gmail.com>
2024-08-15 19:40:16 +0200

    added some props to improve button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,6 +15,10 @@ export default function Button({
   borderRadius,
   width,
   marginVertical,
+  position,
+  bottom,
+  top,
+  height,
 }: {
   title: string;
   onPress: () => void;
@@ -27,6 +31,10 @@ export default function Button({
   borderRadius?: number;
   width?: number;
   marginVertical?: number;
+  position?: 'absolute' | 'relative';
+  bottom?: number;
+  top?: number;
+  height?: number;
 }) {
   return (
     <TouchableOpacity
@@ -42,6 +50,10 @@ export default function Button({
         opacity: disabled ? 0.5 : 1,
         width: width || '100%',
         marginVertical: marginVertical || 0,
+        position: position || 'relative',
+        bottom: bottom || 0,
+        top: top || 0,
+        height: height || 'auto',
       }}
       onPress={onPress}
       disabled={disabled}
