e84c7ab559b3db9a1af0f4de45ec79a389b5d248
poses98 <posesand@gmail.com>
2024-08-16 22:45:05 +0200

    refactor: Update CouponValidation component styles for better user experience

--- components/CouponValidation.tsx
+++ components/CouponValidation.tsx
@@ -147,7 +147,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     zIndex: 5,
-    marginBottom: 20,
+    marginBottom: 30,
   },
   title: {
     fontFamily: 'ArialRounded',
