6a588feff8d68672585d59d7dd72eeb9aa31040a
poses98 <posesand@gmail.com>
2024-08-16 11:28:49 +0200

    refactor: Update settings screen to include "Cambiar contraseña" menu option and FormChangePassword component

--- app/(app)/settings.tsx
+++ app/(app)/settings.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import CustomModal from '@/components/CustomModal';
+import FormChangePassword from '@/components/FormChangePassword';
+import LogoutButton from '@/components/LogoutButton';
+import { ThemedText } from '@/components/ThemedText';
+import { AntDesign } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Settings() {
+  const [isChangePasswordVisible, setIsChangePasswordVisible] = useState(false);
   return (
     <View style={styles.wrapper}>
       <ScrollView contentContainerStyle={styles.container}>
-        <Text>Settings</Text>
+        <TouchableOpacity
+          style={styles.menuOption}
+          onPress={() => setIsChangePasswordVisible(true)}
+        >
+          <Text>Cambiar contraseña</Text>
+          <AntDesign name="right" size={24} color="black" />
+        </TouchableOpacity>
+        <LogoutButton color />
+        <ThemedText
+          type="form-label"
+          style={{
+            color: 'rgba(0,0,0,0.4)',
+            width: '100%',
+            textAlign: 'center',
+            marginTop: 20,
+          }}
+        >
+          {process.env.EXPO_PUBLIC_APP_VERSION} by Pablo Osés
+        </ThemedText>
+        <CustomModal
+          isVisible={isChangePasswordVisible}
+          onClose={() => setIsChangePasswordVisible(false)}
+          title="Cambiar contraseña"
+          isForm
+        >
+          <FormChangePassword
+            onClose={() => {
+              setIsChangePasswordVisible(false);
+            }}
+          />
+        </CustomModal>
       </ScrollView>
     </View>
   );
@@ -25,5 +68,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     padding: 10,
+  },
+  menuOption: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
