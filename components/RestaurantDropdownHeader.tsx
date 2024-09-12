107df0598d0afde5f45d3e12c8cc88608eaf9ae0
poses98 <posesand@gmail.com>
2024-09-12 16:27:21 +0200

    refactor: Update RestaurantDropdownHeader component to display restaurant dropdown headers and handle opening and closing of restaurants

--- components/RestaurantDropdownHeader.tsx
+++ components/RestaurantDropdownHeader.tsx
@@ -7,56 +7,80 @@ import { Colors } from '@/constants/Colors';
 interface RestaurantDropdownHeaderProps {
   restaurantName: string;
   city: string;
+  code: number;
+  onPress?: () => void;
+  onEditPress?: () => void;
 }
 
 export default function RestaurantDropdownHeader({
   restaurantName,
   city,
+  code,
+  onPress,
+  onEditPress,
 }: RestaurantDropdownHeaderProps) {
   const [isOpened, setIsOpened] = useState(false);
 
-  const handleOpen = () => {
+  const handleOnPress = () => {
+    onPress && onPress();
     setIsOpened((prevState) => !prevState);
   };
   return (
-    <TouchableOpacity style={styles.dropdownContainer} onPress={handleOpen}>
-      <AntDesign
-        name={isOpened ? 'caretdown' : 'caretright'}
-        size={12}
-        color={Colors.light.dateCoupon}
-      />
+    <View style={styles.dropdownWrapper}>
+      <TouchableOpacity
+        style={styles.dropdownContainer}
+        onPress={handleOnPress}
+      >
+        <AntDesign
+          name={isOpened ? 'caretdown' : 'caretright'}
+          size={12}
+          color={Colors.light.dateCoupon}
+        />
+        <View>
+          <ThemedText
+            type="default"
+            style={[styles.headerText, { fontWeight: '300', fontSize: 14 }]}
+          >
+            {city}
+          </ThemedText>
+          <ThemedText
+            type="defaultSemiBold"
+            style={[styles.headerText, { fontWeight: 'bold', fontSize: 18 }]}
+          >
+            {`${restaurantName} (#${code})`}
+          </ThemedText>
+        </View>
+      </TouchableOpacity>
       <View>
-        <ThemedText
-          type="default"
-          style={{
-            color: Colors.light.dateCoupon,
-          }}
-        >
-          {city}
-        </ThemedText>
-        <ThemedText
-          type="defaultSemiBold"
-          style={{
-            color: Colors.light.dateCoupon,
-          }}
-        >
-          {restaurantName}
-        </ThemedText>
+        <TouchableOpacity onPress={onEditPress}>
+          <AntDesign name="edit" size={20} color={Colors.light.dateCoupon} />
+        </TouchableOpacity>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  dropdownWrapper: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    marginBottom: 4,
+    borderBottomColor: Colors.light.separatorLine,
+    borderBottomWidth: 1,
+    borderRadius: 11,
+    paddingRight: 25,
+    paddingBottom: 6,
+  },
   dropdownContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
     width: '100%',
-    borderRadius: 11,
-    marginBottom: 4,
-    borderBottomColor: Colors.light.separatorLine,
-    borderBottomWidth: 1,
-    paddingBottom: 6,
+  },
+  headerText: {
+    color: Colors.light.dateCoupon,
+    marginLeft: 4,
+    textTransform: 'uppercase',
   },
 });
